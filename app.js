e7b8e1ab42f9c845e7481a45c6f882f77dace9b0
Mariane Ferreira dos Anjos <marianeferreiradosanjos@MacBook-Pro-de-Mariane.local>
2025-01-19 15:03:39 -0300

    adicao de numeros de tentavias e melhoria da logica com while

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 4;
-console.log('O numero secreto é '+ numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
-console.log ('O chute escolhido pelo jogador foi: '+ chute);
+let numeroSecreto = 5;
+console.log('O numero secreto é '+ numeroSecreto);
+let chute;
+let tentativas = 1;
 
-// Se o chute for igual ao numero secreto 
-if (chute == numeroSecreto ) {
-    alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto}`);    
-} else{ //Se o chute for diferente
-    alert('Voce errou )`:')
+//enquanto o chute nao for igual ao numero secreto
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto ) {
+        alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);    
+    } else {
+        if (chute > numeroSecreto) {
+            alert( `O numero secreto é menor que o ${chute}`);
+        } else{
+            alert(`O numero secreto é maior que o ${chute}`);
+        }
+        tentativas++;
+    }
 }
