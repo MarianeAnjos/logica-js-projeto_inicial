9cf72681c205739f68afa0bb1fa90a38f13389d5
Mariane Ferreira dos Anjos <marianeferreiradosanjos@MacBook-Pro-de-Mariane.local>
2025-01-19 14:28:01 -0300

    Adicao de console log + else

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 4;
 console.log('O numero secreto é '+ numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
+console.log ('O chute escolhido pelo jogador foi: '+ chute);
 
 // Se o chute for igual ao numero secreto 
 if (chute == numeroSecreto ) {
